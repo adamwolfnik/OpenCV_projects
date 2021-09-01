4c24dfc0f426e19eb1c578e8e736a919f9b642a4
NikhilMJain <nikhlmjain1066@gmail.com>
2021-09-01 11:28:06 +0530

    Second Project

--- Virtual_drag_and_drop.py
+++ Virtual_drag_and_drop.py
@@ -50,6 +50,7 @@ while True:
   out=img.copy()
   alpha=0.1
   mask=imgNew.astype(bool)
+  cv2.putText(imgNew," Nikhil ",(320,128),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,2,(200,180,200),2)
   out[mask]=cv2.addWeighted(img, alpha, imgNew,1-alpha, 0)[mask]
 
   cv2.imshow("Image",out)
